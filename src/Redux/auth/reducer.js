4c5e64994ef7edadde4a3f64cb4527ab5b7a36e2
Himanshu <himrd95@gmail.com>
2021-02-28 12:17:27 +0530

    updated

--- src/Redux/auth/reducer.js
+++ src/Redux/auth/reducer.js
@@ -12,7 +12,7 @@ import { loadData, saveData } from "../../Utils"
 
 const aut = loadData("todoAdvance").loggedIn || false
 console.log(aut)
-const tokenUpdated = {auth: "", token: "", username: ""};
+const tokenUpdated = {auth: aut, token: "", username: ""};
 saveData("todoAdvance", tokenUpdated)
 const initState = {
     message: "",
